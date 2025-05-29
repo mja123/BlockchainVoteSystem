49f8da90410191146641b4fee95e0d48d5a47ad4
mja123 <matias.aguilar.sansoni@gmail.com>
2025-05-29 06:27:20 -0300

    feat: adding appUser enrollement

--- src/utils/blockchainConnection.js
+++ src/utils/blockchainConnection.js
@@ -1,16 +1,17 @@
 const { Gateway, Wallets } = require('fabric-network');
 const path = require('path');
 const fs = require('fs');
+const enrollUser = require('./enrollUser.js');
 
 const connectionJson =  process.env.ORG1_CONNECTION_JSON || "../../blockchain/test-network/organizations/peerOrganizations/org1.example.com/connection-org1.json"
 console.log('Using connection JSON: ', connectionJson)
 
 const ccpPath = path.resolve(__dirname, connectionJson);
 const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
-
+enrollUser()
 async function getContract() {
   // 1. Set up wallet and identity
-  const wallet = await Wallets.newFileSystemWallet(path.join(__dirname, 'wallet'));
+  const wallet = await Wallets.newFileSystemWallet(path.join(__dirname, '../wallet'));
   // if (!(await wallet.list()).includes('appUser')) {
   //   throw new Error('appUser identity not found in wallet');
   // }
